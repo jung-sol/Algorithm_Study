850746a2b045cf87e38fdeda919323314c0ba50f
jung sol <soljung0725@gamil.com>
2022-05-16 14:11:23 +0900

    Add Files

---
+++ b001/q2588.py
@@ -0,0 +1,10 @@
+x = input()
+y = input()
+
+# print(int(x) * int(y[-1]))
+# print(int(x) * int(y[-2]))
+# print(int(x) * int(y[-3]))
+# print(int(x) * int(y))
+
+
+print(int(x) * int(y[-1]), int(x) * int(y[-1]), int(x) * int(y[-1]), int(x) * int(y), sep='\n')
